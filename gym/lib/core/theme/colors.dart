500726f766305a0dace4bb11bacb6fedf3532557
ibrahem137 <ibrahemalhuossien@gmail.com>
2025-11-17 16:21:19 +0300

    modify

--- gym/lib/core/theme/colors.dart
+++ gym/lib/core/theme/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-class colorsManegar {
+class ColorsManegar {
   static const Color mainOrange = Color(0xffff3333);
   static const Color semewhite = Color(0xfff2f2f3);
   static const Color darkgray = Color(0xff545454);
-  static const Color darkblur = Color(0xff0A407D);
+  static const Color darkblue = Color(0xff0A407D);
   static const Color lightblue = Color(0xff2B7FFD);
 }
