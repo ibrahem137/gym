83c51eec52167528646acdff74b292950d39914e
ibrahem137 <ibrahemalhuossien@gmail.com>
2025-11-29 17:50:55 +0300

    rebuilding the members screen

--- gym/lib/features/members/ui/screens/members_screen.dart
+++ gym/lib/features/members/ui/screens/members_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gym/core/theme/app_shadow.dart';
 import 'package:gym/core/theme/colors.dart';
 import 'package:gym/core/widgets/home_texts_headers.dart';
-import 'package:gym/features/members/ui/widgets/members_card.dart';
-import 'package:gym/features/members/ui/widgets/members_head_card.dart';
+import 'package:gym/features/dashboard/ui/widgets/members_table/members_table_card.dart';
 import 'package:gym/features/members/ui/widgets/members_search_filed.dart';
 
 class MembersScreen extends StatelessWidget {
@@ -17,27 +15,19 @@ class MembersScreen extends StatelessWidget {
           //----------  Header ----------
           HomeTextsHeaders(
             headerText: "Members",
-            subHeaderText: "Manage your gym members and memberships",
+            subHeaderText:
+                "Manage your gym members and memberships",
           ),
           SizedBox(height: 30),
           MembersSearchFiled(),
+          SizedBox(height: 30),
 
           //----------  Card ----------
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey.shade300),
-                color: ColorsManegar.backgroundcolor,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: AppShadows.bigOuterList,
-              ),
-              height: double.maxFinite,
-              child: Column(
-                //      mainAxisSize: MainAxisSize.min,
-                children: [MembersHeadCard(), const Divider(), MembersCard()],
-              ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30.0,
             ),
+            child: MembersTableCard(),
           ),
         ],
       ),
